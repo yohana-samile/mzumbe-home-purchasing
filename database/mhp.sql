f342128d4654aa74e5d801d65c8d5ef9a93cf4cd
yohana-samile <yohanasamile@gmail.com>
2023-05-27 15:02:39 +0300

    27/05/2023 database commit

--- database/mhp.sql
+++ database/mhp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2023 at 05:53 PM
+-- Generation Time: May 27, 2023 at 02:01 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -54,6 +54,7 @@ CREATE TABLE `product` (
   `soldBy` int(11) NOT NULL,
   `status` varchar(50) NOT NULL DEFAULT '''available''',
   `productCategory` int(11) NOT NULL,
+  `productPrice` int(11) NOT NULL,
   `productImage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -61,14 +62,14 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productId`, `productName`, `soldBy`, `status`, `productCategory`, `productImage`) VALUES
-(1, 't-shirt', 1, 'on stock', 1, 'a137a885bc6bfa50ba496e9203606d79.jpg'),
-(3, 'Mac laptop', 1, 'on stock', 2, 'best laptops1679305053832.jpg'),
-(4, 'shoes', 1, 'on stock', 3, '7pyu4_512.jpg'),
-(5, 'shoes', 1, 'on stock', 3, 'images.jpg'),
-(6, 'shoes', 1, 'on stock', 3, '13364970_15364214_1000.jpg'),
-(7, 'furniture', 1, 'on stock', 4, 'furniture.jpg'),
-(8, 'furniture', 1, 'on stock', 4, '61tV07vXkIL.jpg');
+INSERT INTO `product` (`productId`, `productName`, `soldBy`, `status`, `productCategory`, `productPrice`, `productImage`) VALUES
+(1, 't-shirt', 1, 'on stock', 1, 12000, 'a137a885bc6bfa50ba496e9203606d79.jpg'),
+(3, 'Mac laptop', 1, 'on stock', 2, 2000000, 'best laptops1679305053832.jpg'),
+(4, 'shoes', 1, 'on stock', 3, 25000, '7pyu4_512.jpg'),
+(5, 'shoes', 1, 'on stock', 3, 35000, 'images.jpg'),
+(6, 'shoes', 1, 'on stock', 3, 40000, '13364970_15364214_1000.jpg'),
+(7, 'furniture', 1, 'on stock', 4, 230000, 'furniture.jpg'),
+(8, 'furniture', 1, 'on stock', 4, 400000, '61tV07vXkIL.jpg');
 
 -- --------------------------------------------------------
 
